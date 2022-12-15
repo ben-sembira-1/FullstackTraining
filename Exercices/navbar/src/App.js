dc502b0ca3e5c6469d02f6584fda6e4429ce6241
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-15 23:45:06 +0200

    feat: create basic navbar with items

--- Exercices/navbar/src/App.js
+++ Exercices/navbar/src/App.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 function App() {
   return (
-    <div></div>
+    <Navbar>
+      <Navitem icon="🦷"/>
+      <Navitem icon="🫀"/>
+      <Navitem icon="👀"/>
+    </Navbar>
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>
+        {props.children}
+      </ul>
+    </nav>
+  );
+}
+
+function Navitem(props) {
+  return (
+    <li className='nav-item'>
+      <button href='#' className='item-button'>{props.icon}</button>
+    </li>
   );
 }
 
