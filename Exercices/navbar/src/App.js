51a7c4b8e0323529b8753b7eefbb605babe72d90
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-19 16:03:57 +0200

    feat: sliding menu

--- Exercices/navbar/src/App.js
+++ Exercices/navbar/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+
 import { Navbar, Navitem } from './Navbar';
-import { DropDownLinkItem, DropDownMenu } from './DropDownMenu';
+import { DropDownMenu } from './DropDownMenu';
 import { ReactComponent as AccountIcon } from './icons/account.svg';
 import { ReactComponent as CreateIcon } from './icons/pencil-outline.svg';
 import { ReactComponent as SettingsIcon } from './icons/cog-outline.svg';
-import { ReactComponent as RightChevron } from './icons/chevron-right.svg';
+
 
 function App() {
   return (
@@ -12,10 +13,7 @@ function App() {
       <Navitem icon={<AccountIcon />} />
       <Navitem icon={<CreateIcon />} />
       <Navitem icon={<SettingsIcon />}>
-        <DropDownMenu>
-          <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron />} href='#'>option-1</DropDownLinkItem>
-          <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron />} href='#'>option-2</DropDownLinkItem>
-        </DropDownMenu>
+        <DropDownMenu />
       </Navitem>
     </Navbar>
   );
--- Exercices/navbar/src/DropDownMenu.js
+++ Exercices/navbar/src/DropDownMenu.js
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { CSSTransition } from 'react-transition-group'
+
+import { ReactComponent as RightChevron } from './icons/chevron-right.svg';
+import { ReactComponent as SettingsIcon } from './icons/cog-outline.svg';
 
 export function DropDownMenu(props) {
+
+    const [activeMenu, setActiveMenu] = useState('main-menu');
+
+
+    function DropDownLinkItem(props) {
+        return (
+            <a href={props.href} className='drop-down-link-item ' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+                <span className='item-button'>{props.leftIcon}</span>
+                {props.children}
+                <span className='item-button drop-down-item-right-icon'>{props.rightIcon}</span>
+            </a>
+        );
+    }
+
     return (
         <div className='drop-down-menu'>
-            {props.children}
+            <CSSTransition
+                in={activeMenu === 'main-menu'}
+                unmountOnExit
+                timeout={500}
+                classNames='menu-primary'
+            >
+                <div className='menu'>
+                    <h1>Main menu</h1>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron />} goToMenu='secondary-menu-1'>go to 1</DropDownLinkItem>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron />} goToMenu='secondary-menu-2'>go to 2</DropDownLinkItem>
+                </div>
+            </CSSTransition>
+            <CSSTransition
+                in={activeMenu === 'secondary-menu-1'}
+                unmountOnExit
+                timeout={500}
+                classNames='menu-secondary'
+            >
+                <div className='menu'>
+                    <h2>Menu 1</h2>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron />} goToMenu='main-menu'>back</DropDownLinkItem>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} >Dont press this</DropDownLinkItem>
+                </div>
+            </CSSTransition>
+            <CSSTransition
+                in={activeMenu === 'secondary-menu-2'}
+                unmountOnExit
+                timeout={500}
+                classNames='menu-secondary'
+            >
+                <div className='menu'>
+                    <h2>Menu 2</h2>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron />} goToMenu='main-menu'>back</DropDownLinkItem>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} >Good option</DropDownLinkItem>
+                    <DropDownLinkItem leftIcon={<SettingsIcon />} >Another good option</DropDownLinkItem>
+                </div>
+            </CSSTransition>
         </div>
-    );
-}
-
-export function DropDownLinkItem(props) {
-    return (
-        <a href={props.href} className='drop-down-link-item button'>
-            <span className='item-button'>{props.leftIcon}</span>
-            {props.children}
-            <span className='item-button drop-down-item-right-icon'>{props.rightIcon}</span>
-        </a>
     );
 }
