e51e344f66aa03246c99364b3bf67591a1fe9974
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-19 18:03:43 +0200

    fix: broken transitions

--- Exercices/navbar/src/DropDownMenu.js
+++ Exercices/navbar/src/DropDownMenu.js
@@ -6,44 +6,43 @@ import { ReactComponent as RightChevron } from './icons/chevron-right.svg';
 import { ReactComponent as LeftChevron } from './icons/chevron-left.svg';
 import { ReactComponent as SettingsIcon } from './icons/cog-outline.svg';
 
+function DropDownLinkItem(props) {
+    return (
+        <a href={props.href} className='drop-down-link-item ' onClick={props.onClick}>
+            <span className='item-button'>{props.leftIcon}</span>
+            {props.children}
+            <span className='item-button drop-down-item-right-icon'>{props.rightIcon}</span>
+        </a>
+    );
+}
+
+function TransitionableSubMenu(props) {
+    const [activeMenu, setActiveMenu] = props.activeMenuState
+    return (
+        <CSSTransition
+            in={activeMenu === props.menuId}
+            unmountOnExit
+            timeout={props.timeout || 500}
+            classNames={props.primary ? 'menu-primary' : 'menu-secondary'}
+        >
+            <div className='menu'>
+                <h1>{props.menuLabel}</h1>
+                {
+                    props.transitions.map(
+                        (transition, index) => <DropDownLinkItem key={index} leftIcon={transition.icon || <SettingsIcon />} rightIcon={props.primary ? <RightChevron /> : <LeftChevron />} onClick={() => setActiveMenu(transition.goToMenu)}>{transition.label}</DropDownLinkItem>
+                    )
+                }
+            </div>
+        </CSSTransition>
+    );
+}
+
 export function DropDownMenu(props) {
-
-    const [activeMenu, setActiveMenu] = useState('main-menu');
-
-
-    function DropDownLinkItem(props) {
-        return (
-            <a href={props.href} className='drop-down-link-item ' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-                <span className='item-button'>{props.leftIcon}</span>
-                {props.children}
-                <span className='item-button drop-down-item-right-icon'>{props.rightIcon}</span>
-            </a>
-        );
-    }
-
-    function TransitionableSubMenu(props) {
-        return (
-            <CSSTransition
-                in={activeMenu === props.menuId}
-                unmountOnExit
-                timeout={props.timeout || 500}
-                classNames={props.primary ? 'menu-primary' : 'menu-secondary'}
-            >
-                <div className='menu'>
-                    <h1>{props.menuLabel}</h1>
-                    {
-                        props.transitions.map(
-                            (transition, index) => <DropDownLinkItem leftIcon={transition.icon || <SettingsIcon />} rightIcon={props.primary ? <RightChevron /> : <LeftChevron />} goToMenu={transition.goToMenu}>{transition.label}</DropDownLinkItem>
-                        )
-                    }
-                </div>
-            </CSSTransition>
-        );
-    }
-
+    const activeMenuState = useState('main-menu');
     return (
         <div className='drop-down-menu'>
             <TransitionableSubMenu
+                activeMenuState={activeMenuState}
                 menuId='main-menu'
                 primary
                 menuLabel='Main Menu'
@@ -54,32 +53,26 @@ export function DropDownMenu(props) {
                     ]
                 }
             />
-
-            <CSSTransition
-                in={activeMenu === 'secondary-menu-1'}
-                unmountOnExit
-                timeout={500}
-                classNames='menu-secondary'
-            >
-                <div className='menu'>
-                    <h2>Menu 1</h2>
-                    <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<LeftChevron />} goToMenu='main-menu'>back</DropDownLinkItem>
-                    <DropDownLinkItem leftIcon={<SettingsIcon />} >Dont press this</DropDownLinkItem>
-                </div>
-            </CSSTransition>
-            <CSSTransition
-                in={activeMenu === 'secondary-menu-2'}
-                unmountOnExit
-                timeout={500}
-                classNames='menu-secondary'
-            >
-                <div className='menu'>
-                    <h2>Menu 2</h2>
-                    <DropDownLinkItem leftIcon={<SettingsIcon />} rightIcon={<LeftChevron />} goToMenu='main-menu'>back</DropDownLinkItem>
-                    <DropDownLinkItem leftIcon={<SettingsIcon />} >Good option</DropDownLinkItem>
-                    <DropDownLinkItem leftIcon={<SettingsIcon />} >Another good option</DropDownLinkItem>
-                </div>
-            </CSSTransition>
+            <TransitionableSubMenu
+                activeMenuState={activeMenuState}
+                menuId='secondary-menu-1'
+                menuLabel='Menu 1'
+                transitions={
+                    [
+                        { 'goToMenu': 'main-menu', 'label': 'Back' }
+                    ]
+                }
+            />
+            <TransitionableSubMenu
+                activeMenuState={activeMenuState}
+                menuId='secondary-menu-2'
+                menuLabel='Menu 2'
+                transitions={
+                    [
+                        { 'goToMenu': 'main-menu', 'label': 'Back' }
+                    ]
+                }
+            />
         </div>
     );
 }
