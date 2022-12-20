cd85d712a952ff9f8999f7de43cf6281be61636f
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-20 15:25:57 +0200

    feat: better mock of data

--- Exercices/grocery-store/src/App.js
+++ Exercices/grocery-store/src/App.js
@@ -1,14 +1,24 @@
 import { FilterableProductTable } from './FilterableProductTable'
 
 function fetchAllProducts() {
-  return [
-    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  const products = [];
+  for (let basePrice = 0; basePrice < 3; basePrice++) {
+    [{ category: "Fruits", price: "$1", stocked: true, name: "Apple" },
     { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
     { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
     { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
     { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-  ];
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }].forEach(
+      (value) => {
+        const new_price = (+(value.price[1]) + basePrice);
+        value.price = "$" + new_price;
+        value.stocked = Math.random() > 0.3
+        products.push(value);
+      }
+    )
+    products.push()
+  }
+  return products;
 }
 
 function App() {
