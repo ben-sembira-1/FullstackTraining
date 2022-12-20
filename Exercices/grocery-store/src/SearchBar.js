ff6a0d3758627dacde87e91dfe20f36424b41179
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-20 09:20:11 +0200

    wip: all components are written

--- Exercices/grocery-store/src/SearchBar.js
+++ Exercices/grocery-store/src/SearchBar.js
@@ -2,6 +2,7 @@ export function SearchBar({ filters }) {
     return (
         <>
             <input type="text" label="Search..." />
+            <input type="checkbox" label="Only show products in stock" />
         </>
     );
 }
