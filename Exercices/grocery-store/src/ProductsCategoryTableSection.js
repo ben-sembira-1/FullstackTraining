3fd74d75063c2759cf9861ab766a0c51fdc66430
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-20 09:56:02 +0200

    fix: span subheaders over the row

--- Exercices/grocery-store/src/ProductsCategoryTableSection.js
+++ Exercices/grocery-store/src/ProductsCategoryTableSection.js
@@ -4,7 +4,7 @@ export function ProductsCategoryTableSection({ name, products }) {
     return (
         <>
             <tr>
-                <th className="products-table-full-row">{name}</th>
+                <th className="products_table_full_row" colSpan={2}>{name}</th>
             </tr>
             {
                 products.map(
