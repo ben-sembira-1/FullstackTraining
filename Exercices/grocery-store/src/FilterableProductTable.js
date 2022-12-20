ade55e92f9b1252e1e6d489db277a0e0f860eaac
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-20 09:53:28 +0200

    refactor: move the products up in the hirarchy

--- Exercices/grocery-store/src/FilterableProductTable.js
+++ Exercices/grocery-store/src/FilterableProductTable.js
@@ -1,22 +1,14 @@
 import { SearchBar } from './SearchBar';
 import { ProductsTable } from './ProductsTable';
 
-function fetchAllProducts() {
-    return [
-        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-    ];
-}
+
 
 function filterProducts(products, filters) {
     return [
         { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
         { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
         { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
         { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
     ];
 }
@@ -34,16 +26,16 @@ function categorizeProducts(products) {
             name: "Vegetables",
             products: [
                 { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+                { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
                 { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
             ]
         }
     ];
 }
 
-export function FilterableProductTable({ groceries }) {
+export function FilterableProductTable({ products }) {
 
-    const allProducts = fetchAllProducts();
-    const filterdProducts = filterProducts(allProducts);
+    const filterdProducts = filterProducts(products, [((product) => true)]);
     const categorizedData = categorizeProducts(filterdProducts);
 
     return (
