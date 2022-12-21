1177936c126d2aad4461ab6d6362372eafb51e48
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-21 17:04:19 +0200

    fix: fixed typing for navbar.tsx

--- Exercices/navbar/src/Navbar.tsx
+++ Exercices/navbar/src/Navbar.tsx
@@ -1,30 +1,27 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, FunctionComponent } from 'react'
 import { DropDownMenu } from './DropDownMenu'
+import { Container } from './commonTypes'
 
-export function Navbar ({ children }) {
-  return (
-    <nav className="navbar">
-      <ul className="navbar-nav">{children}</ul>
-    </nav>
-  )
-}
-Navbar.propTypes = {
-  children: PropTypes.arrayOf(Navitem)
+interface NavitemInterface extends Container {
+  icon: React.ReactSVGElement
 }
 
-export function Navitem ({ icon, children }) {
+export function Navitem (props: NavitemInterface) {
   const [open, setOpen] = useState(false)
   return (
     <li className="nav-item">
       <button className="item-button button" onClick={() => setOpen(!open)}>
-        {icon}
+        {props.icon}
       </button>
-      {open && children}
+      {open && props.children}
     </li>
   )
 }
-Navitem.propTypes = {
-  icon: PropTypes.element,
-  children: PropTypes.oneOfType([PropTypes.objectOf(DropDownMenu), PropTypes.arrayOf(PropTypes.objectOf(DropDownMenu))])
+
+export function Navbar (props: Container) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  )
 }
