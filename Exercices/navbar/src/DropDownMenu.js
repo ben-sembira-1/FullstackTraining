b2671c5770207c6b542e704998331fc79e738388
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-21 09:32:50 +0200

    wip: attempt to restracture the code

--- Exercices/navbar/src/DropDownMenu.js
+++ Exercices/navbar/src/DropDownMenu.js
@@ -7,7 +7,7 @@ import { ReactComponent as RightChevron } from './icons/chevron-right.svg'
 import { ReactComponent as LeftChevron } from './icons/chevron-left.svg'
 import { ReactComponent as SettingsIcon } from './icons/cog-outline.svg'
 
-function DropDownLinkItem
+function MenuLinkItem
 ({ onClick, leftIcon, rightIcon, children }) {
   return (
     <a className="drop-down-link-item button" onClick={onClick}>
@@ -17,132 +17,103 @@ function DropDownLinkItem
     </a>
   )
 }
-DropDownLinkItem.propTypes = {
+MenuLinkItem.propTypes = {
   onClick: PropTypes.func,
   leftIcon: PropTypes.shape('svg'),
   rightIcon: PropTypes.shape('svg'),
   children: PropTypes.array.isRequired
 }
 
-function TransitionableSubMenu
-({
-  activeMenuState,
-  menuHeightState,
-  menuId,
-  timeout,
-  primary,
-  menuLabel,
-  transitions,
-  children
-}) {
-  const defaultTimeout = 500
-  // HELP:    Is this state propegation a good thing to do?
-  //          It feels to me that very fast this aproach can end in lots of states being propegated
-  //          through lots of layers of components.
-  const [activeMenu, setActiveMenu] = activeMenuState
-  const [, setMenuHeight] = menuHeightState
-
-  function calculateHeight
-  (htmlElement) {
-    setMenuHeight(htmlElement.offsetHeight)
-  }
-
+function Menu ({ header, children }) {
   return (
-    <CSSTransition
-      in={activeMenu === menuId}
-      unmountOnExit
-      timeout={timeout || defaultTimeout}
-      classNames={primary ? 'menu-primary' : 'menu-secondary'}
-      onEnter={calculateHeight}
-    >
-      <div className="menu">
-        <h1>{menuLabel}</h1>
-        {transitions.map((transition, index) => (
-          <DropDownLinkItem
-            key={index}
-            leftIcon={transition.icon || <SettingsIcon />}
-            rightIcon={primary ? <RightChevron /> : <LeftChevron />}
-            onClick={() => setActiveMenu(transition.goToMenu)}
-          >
-            {transition.label}
-          </DropDownLinkItem>
-        ))}
-        {children}
-      </div>
-    </CSSTransition>
+    <div className="menu">
+      <h2>{header}</h2>
+      {children}
+    </div>
   )
 }
-TransitionableSubMenu.propTypes = {
-  activeMenuState: PropTypes.array,
-  menuHeightState: PropTypes.array,
-  menuId: PropTypes.string,
-  timeout: PropTypes.number,
-  primary: PropTypes.bool,
-  menuLabel: PropTypes.string,
-  transitions: PropTypes.arrayOf([
-    PropTypes.shape({
-      goToMenu: PropTypes.string,
-      label: PropTypes.string
-    })
-  ]),
-  children: PropTypes.arrayOf(DropDownLinkItem)
+Menu.propTypes = {
+  header: PropTypes.string,
+  children: PropTypes.array
 }
 
 export function DropDownMenu
 () {
-  const activeMenuState = useState('main-menu')
-  const menuHeightState = useState(null)
+  const [activeMenu, setActiveMenu] = useState('main-menu')
+  const [menuHeight, setMenuHeight] = useState(null)
   const dropDownMenu = useRef(null)
-  const [menuHeight, setMenuHeight] = menuHeightState
   // This useEffect is for the first transition
   useEffect(() => {
     const menuComputedStyle = getComputedStyle(dropDownMenu.current)
     setMenuHeight(menuComputedStyle.height)
   }, [setMenuHeight])
+
+  const MainMenu = () => {
+    <Menu header='Main Menu'>
+      <MenuLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron/>} onClick={() => setActiveMenu('secondary-menu-1')}>Go to 1</MenuLinkItem>
+      <MenuLinkItem leftIcon={<SettingsIcon />} rightIcon={<RightChevron/>} onClick={() => setActiveMenu('secondary-menu-2')}>Go to 2</MenuLinkItem>
+    </Menu>
+  }
+
+  const SubMenu1 = () => {
+    <Menu header='Menu 1'>
+      <MenuLinkItem leftIcon={<SettingsIcon />} rightIcon={<LeftChevron/>} onClick={() => setActiveMenu('main-menu')}>back</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+    </Menu>
+  }
+
+  const SubMenu2 = () => {
+    <Menu header='Menu 2'>
+      <MenuLinkItem leftIcon={<SettingsIcon />} rightIcon={<LeftChevron/>} onClick={() => setActiveMenu('main-menu')}>back</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+      <MenuLinkItem>Regular Option</MenuLinkItem>
+    </Menu>
+  }
   // HELP: I failed to make the drop down list open with a transition (as if it was with height 0 and then opend).
   // HELP: I failed to create a closure that creates a TransitionableSubMenu with the 2 states.
+  const timeout = 500
+  const calculateHeight = (htmlElement) => setMenuHeight(htmlElement.offsetHeight)
   return (
     <div
       ref={dropDownMenu}
       className="drop-down-menu"
       style={{ height: menuHeight }}
     >
-      <TransitionableSubMenu
-        activeMenuState={activeMenuState}
-        menuHeightState={menuHeightState}
-        menuId="main-menu"
-        primary
-        menuLabel="Main Menu"
-        transitions={[
-          { goToMenu: 'secondary-menu-1', label: 'Go to 1' },
-          { goToMenu: 'secondary-menu-2', label: 'Go to 2' }
-        ]}
-      />
-      <TransitionableSubMenu
-        activeMenuState={activeMenuState}
-        menuHeightState={menuHeightState}
-        menuId="secondary-menu-1"
-        menuLabel="Menu 1"
-        transitions={[{ goToMenu: 'main-menu', label: 'Back' }]}
-      >
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-      </TransitionableSubMenu>
-      <TransitionableSubMenu
-        activeMenuState={activeMenuState}
-        menuHeightState={menuHeightState}
-        menuId="secondary-menu-2"
-        menuLabel="Menu 2"
-        transitions={[{ goToMenu: 'main-menu', label: 'Back' }]}
-      >
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-        <DropDownLinkItem>Regular Option</DropDownLinkItem>
-      </TransitionableSubMenu>
+      <CSSTransition
+      in={activeMenu === 'main-menu'}
+      unmountOnExit
+      timeout={timeout}
+      classNames={'menu-primary'}
+      onEnter={calculateHeight}
+      className='menu-transition'
+    >
+      <MainMenu/>
+      </CSSTransition>
+      <CSSTransition
+      in={activeMenu === 'secondary-menu-1'}
+      unmountOnExit
+      timeout={timeout}
+      classNames={'menu-secondary'}
+      onEnter={calculateHeight}
+      className='menu-transition'
+    >
+      <SubMenu1/>
+      </CSSTransition>
+      <CSSTransition
+      in={activeMenu === 'secondary-menu-2'}
+      unmountOnExit
+      timeout={timeout}
+      classNames={'menu-secondary'}
+      onEnter={calculateHeight}
+      className='menu-transition'
+    >
+      <SubMenu2/>
+    </CSSTransition>
     </div>
   )
 }
