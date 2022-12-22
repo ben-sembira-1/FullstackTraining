815c3a3c9efde789b19f086aa43782c9200a8fbe
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-22 12:14:11 +0200

    fix: make string format more readable

--- Exercices/grocery-store/src/ProductTableRow.tsx
+++ Exercices/grocery-store/src/ProductTableRow.tsx
@@ -9,7 +9,7 @@ export const ProductTableRow: FunctionComponent<ProductTableRowProps> = (props) 
   return (
         <tr className={props.product.stocked ? 'product_row--in_stock' : 'product_row--out_of_stock'}>
             <td>{props.product.name}</td>
-            <td>{`$${props.product.priceDollars}`}</td>
+            <td>{'$' + props.product.priceDollars.toString()}</td>
         </tr>
   )
 }
