a4ee8f861fb573e1afdfe22dbfddda28a332d4ac
ben-sembira-1 <ben.sembira.1@gmail.com>
2022-12-28 10:38:07 +0200

    feat: center main div

--- Exercises/quote-me/src/login/Login.tsx
+++ Exercises/quote-me/src/login/Login.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import { Logo } from '../logo/Logo'
+import './Login.css'
 
 const Login: FunctionComponent = () => {
   return (
-    <>
+    <div className="login_screen">
       <Logo />
-    </>
+      <Logo />
+    </div>
   )
 }
 
