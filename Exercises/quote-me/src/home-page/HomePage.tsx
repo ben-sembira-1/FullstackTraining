1ff5c28773536008c0c5772fc8b7529091858dbe
ben-sembira-1 <ben.sembira.1@gmail.com>
2023-01-03 10:50:06 +0200

    feat: home page grid

--- Exercises/quote-me/src/home-page/HomePage.tsx
+++ Exercises/quote-me/src/home-page/HomePage.tsx
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react'
-import { ConnectedUser } from '../contexts'
+import React from 'react'
+import './HomePage.css'
+
+const TopRow = () => <h1>Top Row Here</h1>
+const HallOfFame = () => <h1>The Most Iconic Here</h1>
+const Feed = () => <h1>Feed Here</h1>
 
 const HomePage = () => {
-  const connectedUser = useContext(ConnectedUser)
   return (
-    <div>
-      <h1>I am the home page</h1>
-      <article style={{ width: '300px' }}>
-        {JSON.stringify(connectedUser)}
-      </article>
+    <div className='home_page'>
+      <div className='top_row'>
+        <TopRow/>
+      </div>
+      <div className='hall_of_fame'>
+        <HallOfFame/>
+      </div>
+      <div className='feed'>
+        <Feed/>
+      </div>
     </div>
   )
 }
