7e835699e56aad2be1aef87b81ff364fa26bf3cc
ben-sembira-1 <ben.sembira.1@gmail.com>
2023-01-03 16:00:17 +0200

    feat: new quote button

--- Exercises/quote-me/src/home-page/top-row/TopRow.tsx
+++ Exercises/quote-me/src/home-page/top-row/TopRow.tsx
@@ -13,6 +13,7 @@ const TopRow = () => {
       <div className='top_row-phrase'>
         May the most bizarre, delusional and mad person win.
       </div>
+      <button className='top_row-add_new_button'>Add New Quote</button>
       <div className='top_row-user_thumbnail'>
         <UserThumbnail/>
       </div>
